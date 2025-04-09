f4d979afa5cebc87183ac51073762b12999fcbc3
CesarCaO <ccastelazo8@gmail.com>
2025-04-08 22:19:24 -0600

    pruebas de actualización

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI() #objeto que instancia Fast API
 
 @app.get("/")#Cuando alguien llame a ala ruta raíz entonces haz lo que viene en la línea inmediata
 def index():
-     return {"saludo": "Hola fokin mundo"} #fast api ya jsonifica el retorno
+     return {"saludo": "HOLA MUNDO"} #fast api ya jsonifica el retorno
