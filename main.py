7fb61bd44cbd8a7728b010f1c4031c49933ea1f5
CesarCaO <ccastelazo8@gmail.com>
2025-04-27 21:20:02 -0600

    Primera prueba de envió de un archivo json a traves de la api

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI() #objeto que instancia Fast API
 
 @app.get("/")#Cuando alguien llame a ala ruta raíz entonces haz lo que viene en la línea inmediata
 def index():
      return {"saludo": "HOLA MUNDO"} #fast api ya jsonifica el retorno
+
+@app.post("/title_size")
+def title_size():
+     with open("./JSON_Metrics/Title_size.json", "r") as file:
+          data=json.load(file)
+          data['current_document']=17
+     return data
