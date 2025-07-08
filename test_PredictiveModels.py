16de87656dd631dfd21ab41c8a6ff23b2fbd18b6
CesarCaO <ccastelazo8@gmail.com>
2025-07-07 22:16:37 -0600

    Staring to test predictive models with real papers (cleaning and preprocessing the text are required)

--- test_PredictiveModels.py
+++ test_PredictiveModels.py
@@ -4,8 +4,14 @@ import pickle
 import re, time
 from nltk import word_tokenize, sent_tokenize
 from sklearn.metrics import accuracy_score, f1_score, classification_report, confusion_matrix
+import os
+import fitz
 
 
+pdfs = "./PDFs/test_papers/"
+langFile = os.listdir(pdfs)
+text=""
+"""
 #*Open the two datasets with PeerRead (df1) and ASAP (df2)
 #path_1="./CSVs/INCOMPLETO_BOW_Completo.csv"
 path_2="./CSVs/Particiones_COMPLETO.csv"
@@ -18,6 +24,7 @@ df2=pd.read_csv(path_2, encoding="utf-8")
 
 print("Dataset 2",df2.columns)
 print(df2["Conferencia"].unique())
+"""
 
 
 # === Funciones auxiliares ===
@@ -172,4 +179,56 @@ with open(R"./predictive_model/2_EXPEI_ASAP.pkl","rb") as file2:
     F1: {f1_score(y_te, y_pred, average='macro') * 100:.4f}%")
 """
 
-#* Testi
+#* Testing models with 8 real papers
+
+
+
+model_files=[
+    r"./predictive_model/2_EXPEI_PEER_READ.pkl",
+    r"./predictive_model/2_EXPEI_ASAP.pkl"
+]
+
+for file in model_files:
+    print(f"\n Loading model from: {file}")
+
+    with open(file,"rb") as f:
+        print("Reading Model")
+        data = pickle.load(f)
+    
+    model = data['model']
+    vectorizer = data['vectorizer']
+    label_encoder = data['label_encoder']
+    vocabulario = data['vocabulario']
+    caracteristicas = data['caracteristicas']
+    pronombres = ['we', 'our', 'us']
+
+    for pdf in langFile:
+        pdf_path=os.path.join(pdfs,pdf)
+        doc = fitz.open(pdf_path)
+        print("Extracting text")
+        for page in doc:
+            text+=page.get_text().encode('utf-8').decode('utf-8',errors='ignore')
+        doc.close()
+
+        x_test =[text.lower()]
+        print("Vectorizing...")
+        X_test = vectorizer.transform(x_test)
+
+        print("Calculating Expei")
+        fpT, fnpT, contadorPT, contadorNPT = frases(x_test, pronombres, X_test, vocabulario, caracteristicas)
+        peit, _ = PeiNei(contadorPT, fpT, contadorNPT, fnpT, X_test)
+        expei_test = expei_func(X_test, peit)
+
+        print("Making prediction...")
+        y_pred = model.predict(expei_test)
+
+        print(f"{pdf_path}  prediction: ", y_pred)
+
+
+  
+
+
+    
+    
+    
+    
